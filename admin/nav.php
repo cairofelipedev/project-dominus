71905837d339a55e78134028e91e9905e0cb7bcc
Cairo Felipe <cairo.felipe@paraiba.email>
2021-08-26 16:32:40 -0300

    Fixes Layout

--- admin/nav.php
+++ admin/nav.php
@@ -1,105 +1,105 @@
 <div class="sidebar" data-color="green">
-      <!--
+  <!--
         Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
     -->
-      <div class="logo">
-        <a href="../home" class="simple-text logo-normal">
-          <img class="img-fluid" src="../assets/images/logo3.png">
+  <div class="logo">
+    <a href="../home" class="simple-text logo-normal">
+      <img class="img-fluid" src="../assets/images/logo3.png">
+    </a>
+  </div>
+  <div class="sidebar-wrapper" id="sidebar-wrapper">
+    <ul class="nav">
+      <li class="active ">
+        <a href="painel-controle.php">
+          <i class="bi bi-bullseye"></i>
+          <p>Painel de Controle</p>
         </a>
+      </li>
+      <li>
+        <a href="painel-leads.php">
+          <i class="now-ui-icons files_single-copy-04"></i>
+          <p>Leads</p>
+        </a>
+      </li>
+      <li>
+        <a href="painel-produtos.php">
+          <i class="now-ui-icons shopping_cart-simple"></i>
+          <p>Produtos</p>
+        </a>
+      </li>
+      <li>
+        <a href="painel-quemsomos.php?edit_id=1">
+          <i class="now-ui-icons files_paper"></i>
+          <p>Quem somos</p>
+        </a>
+      </li>
+      <li>
+        <a href="painel-blog.php">
+          <i class="now-ui-icons media-1_album"></i>
+          <p>Blog</p>
+        </a>
+      </li>
+      <li>
+        <a href="painel-clientes.php">
+          <i class="fas fa-hands-helping"></i>
+          <p>Clientes</p>
+        </a>
+      </li>
+      <li>
+        <a href="painel-users.php">
+          <i class="now-ui-icons users_single-02"></i>
+          <p>Usuários</p>
+        </a>
+      </li>
+      <li>
+        <a href="painel-whats.php?edit_id=1">
+          <i class="fab fa-whatsapp"></i>
+          <p>Whats</p>
+        </a>
+      </li>
+    </ul>
+  </div>
+</div>
+<div class="main-panel" id="main-panel">
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
+    <div class="container-fluid">
+      <div class="navbar-wrapper">
+        <div class="navbar-toggle">
+          <button type="button" class="navbar-toggler">
+            <span class="navbar-toggler-bar bar1"></span>
+            <span class="navbar-toggler-bar bar2"></span>
+            <span class="navbar-toggler-bar bar3"></span>
+          </button>
+        </div>
+        <a class="navbar-brand" href="painel-controle.php">Painel de Controle</a>
       </div>
-      <div class="sidebar-wrapper" id="sidebar-wrapper">
-        <ul class="nav">
-          <li class="active ">
-            <a href="painel-controle.php">
-            <i class="bi bi-bullseye"></i>
-              <p>Painel de Controle</p>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-bar navbar-kebab"></span>
+        <span class="navbar-toggler-bar navbar-kebab"></span>
+        <span class="navbar-toggler-bar navbar-kebab"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navigation">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="../home" target="_blank">
+              <i class="now-ui-icons media-1_button-play"></i>
             </a>
           </li>
-          <li>
-            <a href="painel-leads.php">
-              <i class="now-ui-icons files_single-copy-04"></i>
-              <p>Leads</p>
-            </a>
-          </li>
-          <li>
-            <a href="painel-produtos.php">
-              <i class="now-ui-icons shopping_cart-simple"></i>
-              <p>Produtos</p>
-            </a>
-          </li>
-          <li>
-            <a href="painel-quemsomos.php?edit_id=1">
-              <i class="now-ui-icons files_paper"></i>
-              <p>Quem somos</p>
-            </a>
-          </li>
-          <li>
-            <a href="painel-blog.php">
-              <i class="now-ui-icons media-1_album"></i>
-              <p>Blog</p>
-            </a>
-          </li>
-          <li>
-            <a href="painel-clientes.php">
-            <i class="fas fa-hands-helping"></i>
-              <p>Clientes</p>
-            </a>
-          </li>
-          <li>
-            <a href="painel-users.php">
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <i class="now-ui-icons users_single-02"></i>
-              <p>Usuários</p>
+              <p>
+                <span class=" d-md-block"><?php echo $_SESSION['name']; ?></span>
+              </p>
             </a>
-          </li>
-          <li>
-            <a href="painel-whats.php?edit_id=1">
-            <i class="fab fa-whatsapp"></i>
-              <p>Whats</p>
-            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item text-info" href="edit-user.php?edit_id=<?php echo $_SESSION['id']; ?>">Editar Perfil</a>
+              <a class="dropdown-item text-danger" href="logout.php">Sair</a>
+            </div>
           </li>
         </ul>
       </div>
     </div>
-    <div class="main-panel" id="main-panel">
-      <!-- Navbar -->
-      <nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
-        <div class="container-fluid">
-          <div class="navbar-wrapper">
-            <div class="navbar-toggle">
-              <button type="button" class="navbar-toggler">
-                <span class="navbar-toggler-bar bar1"></span>
-                <span class="navbar-toggler-bar bar2"></span>
-                <span class="navbar-toggler-bar bar3"></span>
-              </button>
-            </div>
-            <a class="navbar-brand" href="painel-controle.php">Painel de Controle</a>
-          </div>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-bar navbar-kebab"></span>
-            <span class="navbar-toggler-bar navbar-kebab"></span>
-            <span class="navbar-toggler-bar navbar-kebab"></span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-end" id="navigation">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="../home" target="_blank">
-                  <i class="now-ui-icons media-1_button-play"></i>
-                </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <i class="now-ui-icons users_single-02"></i>
-                  <p>
-                    <span class=" d-md-block"><?php echo $_SESSION['name']; ?></span>
-                  </p>
-                </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                  <a class="dropdown-item text-info" href="edit-user.php?edit_id=<?php echo $_SESSION['id']; ?>">Editar Perfil</a>
-                  <a class="dropdown-item text-danger" href="logout.php">Sair</a>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <!-- End Navbar -->
+  </nav>
+  <!-- End Navbar -->
